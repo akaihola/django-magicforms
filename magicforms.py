7c7882257fe6877cb31f5c20026459f7159c4a83
Antti Kaihola <akaihol+github@ambitone.com>
2008-09-23 14:47:26 +0300

    Added unit tests

--- magicforms.py
+++ magicforms.py
@@ -1,6 +1,6 @@
 from Crypto.Cipher import ARC4
 import cPickle as pickle
-from datetime import datetime, timedelta
+import datetime
 from base64 import urlsafe_b64encode as b64encode
 from base64 import urlsafe_b64decode as b64decode
 
@@ -16,6 +16,40 @@ MIN_WAIT_SECONDS = 5
 MAX_WAIT_SECONDS = 3600
 
 def clean_magic(self):
+    """
+    >>> correct_magic = 'xVKiLYj38dNcosqplrmcU4o9AtCJuvVqeg8nwLkfJ2vWlHqkzDMd0SmOkLWky0Pn_B_58OTAOp0xq5VJdYkqfO9-umnQd3KgO7iPWl6psSVxK0PGfCEbQKsgq22Zd55jKFt99ItWj592F_Ba1hHaIyRgMJDNi292KwxcSA8qAwNWjqFuPlCLx1STft6BWciS'
+    >>> when_loaded = datetime.datetime(1991, 10, 5, 18, 53, 0)
+
+    >>> def test_clean(ip, uid, elapsed_secs, magic):
+    ...     class f:
+    ...         remote_ip = ip
+    ...         unique_id = uid
+    ...         cleaned_data = {'magic': magic}
+    ...     datetime._fake_now = when_loaded + datetime.timedelta(seconds=elapsed_secs)
+    ...     clean_magic(f)
+
+    >>> test_clean('1.2.3.4', 16, 60, correct_magic)
+
+    >>> test_clean('1.2.3.4', 16, 2, correct_magic)
+    Traceback (most recent call last):
+    ValidationError: [u'Wait for another 3.00 seconds before submitting this form']
+
+    >>> test_clean('1.2.3.4', 16, 3660, correct_magic)
+    Traceback (most recent call last):
+    ValidationError: [u'This form has expired. Reload the page to get a new one']
+
+    >>> test_clean('1.2.3.5', 16, 60, correct_magic)
+    Traceback (most recent call last):
+    ValidationError: [u'Invalid security token']
+
+    >>> test_clean('1.2.3.4', 17, 60, correct_magic)
+    Traceback (most recent call last):
+    ValidationError: [u'Invalid security token']
+
+    >>> test_clean('1.2.3.4', 16, 60, 'wrong magic')
+    Traceback (most recent call last):
+    ValidationError: [u'Invalid security token']
+    """
     m = self.cleaned_data['magic']
     arc4 = ARC4.new(settings.SECRET_KEY)
     try:
@@ -31,25 +65,36 @@ def clean_magic(self):
         raise forms.ValidationError(_('Invalid security token'))
 
     try:
-        curdelta = datetime.now() - before
+        curdelta = datetime.datetime.now() - before
     except TypeError:
         raise forms.ValidationError(_('Invalid security token'))
 
-    mindelta = timedelta(seconds=MIN_WAIT_SECONDS)
+    mindelta = datetime.timedelta(seconds=MIN_WAIT_SECONDS)
     if curdelta < mindelta:
         d = mindelta - curdelta
         raise forms.ValidationError(_('Wait for another %.2f seconds before submitting this form') % (d.seconds + float(d.microseconds)/1000000))
 
-    if curdelta > timedelta(seconds=MAX_WAIT_SECONDS):
+    if curdelta > datetime.timedelta(seconds=MAX_WAIT_SECONDS):
         raise forms.ValidationError(_('This form has expired. Reload the page to get a new one'))
 
     return m
 
 def set_initial_magic(self):
+    """
+    >>> class f:
+    ...     remote_ip = '1.2.3.4'
+    ...     unique_id = 16
+    ...     data = {}
+    ...     initial = {}
+    >>> datetime._fake_now = datetime.datetime(1991, 10, 5, 18, 53, 0)
+    >>> set_initial_magic(f)
+    >>> f.initial['magic']
+    'xVKiLYj38dNcosqplrmcU4o9AtCJuvVqeg8nwLkfJ2vWlHqkzDMd0SmOkLWky0Pn_B_58OTAOp0xq5VJdYkqfO9-umnQd3KgO7iPWl6psSVxK0PGfCEbQKsgq22Zd55jKFt99ItWj592F_Ba1hHaIyRgMJDNi292KwxcSA8qAwNWjqFuPlCLx1STft6BWciS'
+    """
     if not self.data.get('magic'):
         arc4 = ARC4.new(settings.SECRET_KEY)
         data = {
-            'curtime': datetime.now(),
+            'curtime': datetime.datetime.now(),
             'remote_ip': self.remote_ip,
             'unique_id': self.unique_id,
         }
@@ -57,6 +102,37 @@ def set_initial_magic(self):
         self.initial['magic'] = b64encode(arc4.encrypt(plain))
 
 class MagicForm(forms.Form):
+    """
+    >>> datetime._fake_now = when_loaded = datetime.datetime(1991, 10, 5, 18, 53, 0)
+    >>> correct_magic = 'xVKiLYj38dNcosqplrmcU4o9AtCJuvVqeg8nwLkfJ2vWlHqkzDMd0SmOkLWky0Pn_B_58OTAOp0xq5VJdYkqfO9-umnQd3KgO7iPWl6psSVxK0PGfCEbQKsgq22Zd55jKFt99ItWj592F_Ba1hHaIyRgMJDNi292KwxcSA8qAwNWjqFuPlCLx1STft6BWciS'
+
+    >>> f = MagicForm('1.2.3.4', 16)
+    >>> print f
+    <tr><th></th><td><input id="id_author_bogus_name" style="display:none" type="text" name="author_bogus_name" maxlength="0" /><input type="hidden" name="magic" value="xVKiLYj38dNcosqplrmcU4o9AtCJuvVqeg8nwLkfJ2vWlHqkzDMd0SmOkLWky0Pn_B_58OTAOp0xq5VJdYkqfO9-umnQd3KgO7iPWl6psSVxK0PGfCEbQKsgq22Zd55jKFt99ItWj592F_Ba1hHaIyRgMJDNi292KwxcSA8qAwNWjqFuPlCLx1STft6BWciS" id="id_magic" /></td></tr>
+
+    >>> def test_form(remote_ip, unique_id, elapsed_secs, magic):
+    ...     datetime._fake_now = when_loaded + datetime.timedelta(seconds=elapsed_secs)
+    ...     f = MagicForm(remote_ip, unique_id, data={'magic': magic})
+    ...     return f.is_valid(), f.errors
+
+    >>> test_form('1.2.3.4', 16, 60, correct_magic)
+    (True, {})
+
+    >>> test_form('1.2.3.4', 16, 2, correct_magic)
+    (False, {'magic': [u'Wait for another 3.00 seconds before submitting this form']})
+
+    >>> test_form('1.2.3.4', 16, 3660, correct_magic)
+    (False, {'magic': [u'This form has expired. Reload the page to get a new one']})
+
+    >>> test_form('1.2.3.5', 16, 60, correct_magic)
+    (False, {'magic': [u'Invalid security token']})
+
+    >>> test_form('1.2.3.5', 17, 60, correct_magic)
+    (False, {'magic': [u'Invalid security token']})
+
+    >>> test_form('1.2.3.5', 16, 60, 'wrong magic')
+    (False, {'magic': [u'Invalid security token']})
+    """
     magic = forms.CharField(max_length=1024, widget=forms.HiddenInput())
     author_bogus_name = forms.CharField(required=False, max_length=0, label='', widget=forms.TextInput(attrs={ 'style': 'display:none'}))
 
@@ -82,3 +158,14 @@ class MagicModelForm(forms.ModelForm):
     def clean_magic(self):
         return clean_magic(self)
 
+if __name__ == '__main__':
+    from django.conf import settings
+    settings.configure(SECRET_KEY='secret')
+
+    global _fake_now
+    class FakeDateTime(datetime.datetime):
+        now = staticmethod(lambda: datetime._fake_now)
+    datetime.datetime = FakeDateTime
+
+    from doctest import testmod
+    testmod()
